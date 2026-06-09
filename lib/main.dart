cdaa0fde0f8a98251fb7bba1e1c08444652fe8f9
enockdeghost <enockdeghost@gmail.com>
2026-06-09 17:11:59 +0300

    backGroundColor

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-      title: Text('web flutter'),
+      title: Text('web flutter', style: TextStyle(color: Colors.white),),
+      centerTitle: true,
+      backgroundColor: Colors.deepOrangeAccent,
     ),
+
+    body: Center(
+      child: Text('Hello World'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text('now '),
+    ),
+    backgroundColor: Colors.red[600],
+      
   )
 ));
