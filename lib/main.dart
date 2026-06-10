efc9223fab95729839d9b3fbc71a5b2104abfd6b
enockdeghost <enockdeghost@gmail.com>
2026-06-10 17:58:20 +0300

    clasesless

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,48 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    
+      home: Home(),
+    );
+  }
+}
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('chatApp',style: TextStyle(color: Colors.white)),
-      backgroundColor: Colors.blue,
-    ),
-    body: Center(
-      child: Text('welcome flutter Appp', style: TextStyle(
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold,
-        color: Colors.blue
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Chat App',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
       ),
+      body: const Center(
+        child: Text(
+          'Welcome Flutter App',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
       ),
-    ),
-  ),
-      
-));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Text('+'),
+      ),
+    );
+  }
+}
